6bd4f0fbbd70e9bed6f54a307befc59b4a82dd33
nadin3amanda <nadine@soar.earth>
2023-02-07 19:25:50 +0800

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FormEvent, useState } from 'react';
+
 import './App.css';
+import { Bug, BugPriority } from './Bug';
+import {v4 as uuid} from 'uuid';
+
+
 
 function App() {
+  const [newBugDescription, setNewBugDescription] = useState<string>('');
+  const [newBugPriority, setNewBugPriority] = useState<string>('Low');
+  const [bugList, setBugList] = useState<Bug[]>([]);
+
+  const addBug = (event: FormEvent) => {
+    event.preventDefault();
+    const newBug: Bug = {
+      id: uuid(),
+      description: newBugDescription,
+      priority: newBugPriority as BugPriority,
+    }
+
+    setBugList([
+      ...bugList,
+      newBug
+    ]);
+
+    setNewBugDescription('');
+    setNewBugPriority('Medium');
+  };
+  const deleteBug = (id: string) => {
+    const bugs = bugList.filter(bug => bug.id !== id);
+
+    setBugList(bugs);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1> Lady Bug Tracker </h1>
+     <form onSubmit={addBug}>
+      <label htmlFor='newBugDescription'>
+        New Bug Description:
+        <input type="text" id="newBugDescription" onChange={event => setNewBugDescription(event.target.value)} value={newBugDescription}></input>
+      </label>
+      <label htmlFor='newBugPriority'>
+        New Bug Priority:
+       <select id="newBugPriority" value={newBugPriority} onChange={event => setNewBugPriority(event.target.value)}>
+        <option value="Low">Low</option>
+        <option value="Medium">Medium</option>
+        <option value="High">High</option>
+       </select>
+       <button type="submit">Add New Bug</button>
+      </label>
+     </form>
     </div>
   );
 }
