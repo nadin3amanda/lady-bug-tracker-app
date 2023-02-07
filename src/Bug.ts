42fc93df5d0e3a187189bcd85014036a7a9036f8
nadin3amanda <nadine@soar.earth>
2023-02-07 19:38:59 +0800

    adds files

---
+++ src/Bug.ts
@@ -0,0 +1,11 @@
+export interface Bug {
+    id: string;
+    description: string;
+    priority: BugPriority
+}
+
+export enum BugPriority {
+    LOW = 'Low',
+    MEDIUM = 'Medium',
+    HIGH = 'High'
+}
