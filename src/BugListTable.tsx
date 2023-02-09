b8dfdae3018295a30c26db1293da8d7944272ab9
nadin3amanda <nadine@soar.earth>
2023-02-09 17:56:43 +0800

    Updates

--- src/BugListTable.tsx
+++ src/BugListTable.tsx
@@ -1,4 +1,5 @@
 import { Bug } from "./Bug";
+import styled  from "styled-components";
 
 const BugListTable = (props: {bugs: Bug[], onDeleteBug: Function }) => {
     const {bugs, onDeleteBug} = props;
@@ -10,8 +11,8 @@ const BugListTable = (props: {bugs: Bug[], onDeleteBug: Function }) => {
     return <table>
         <thead>
             <tr>
-                <th>Description</th>
-                <th>Priority</th>
+            <th><Header>Description</Header></th>
+                <th><Header>Priority</Header></th>
                 <th></th>
             </tr>
         </thead>
@@ -20,7 +21,7 @@ const BugListTable = (props: {bugs: Bug[], onDeleteBug: Function }) => {
             {bugs.length > 0 && bugs.map(bug => <tr key={bug.id}>
                 <td>{bug.description}</td>
                 <td>{bug.priority}</td>
-                <td><button onClick={() => resolvedPressed(bug.id)}>Resolved</button></td>
+                <td><ResolvedButton onClick={() => resolvedPressed(bug.id)}>Resolved</ResolvedButton></td>
             </tr>)}
         </tbody>
     </table>
@@ -28,3 +29,21 @@ const BugListTable = (props: {bugs: Bug[], onDeleteBug: Function }) => {
 };
 
 export default BugListTable;
+
+const Header = styled.p`
+    margin: 5px auto;
+    text-align: center;
+    font-size: 15px;
+`
+
+const ResolvedButton = styled.button`
+    background-color: cadetblue;
+    border: none;
+    border-radius: 8px;
+    padding: 10px;
+    color: white;
+    text-transform:uppercase;
+    display: block;
+    margin: 6px auto;
+    cursor: pointer;
+`
